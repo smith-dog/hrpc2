6117bfc8bf6894b495dbdabcb2e81275475160b3
smith.ji <smith.ji@pkfare.com>
2019-11-07 20:29:10 +0800

    Initial commit

---
+++ src/main/java/com/smith/hrpc2/Hrpc2Application.java
@@ -0,0 +1,13 @@
+package com.smith.hrpc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hrpc2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Hrpc2Application.class, args);
+  }
+
+}
